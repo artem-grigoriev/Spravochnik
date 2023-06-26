dcb016eaf79a451ab3bb985cf6b4ed30ef86ada7
Artem <googl.dr.02.12345@gmail.com>
2023-06-26 13:01:41 +0300

    fix

--- Spravochnik/Models/Worker.cs
+++ Spravochnik/Models/Worker.cs
@@ -9,37 +9,5 @@
         public int Height { get; set; }
         public DateTime BirthDate { get; set; }
         public string BirthPlace { get; set; }
-        public static Worker AddWorker(int id, DateTime dateAdd)
-        {
-            Console.WriteLine("Введите через запятую(после запятой пробел не ставить) ФИО, рост, дату и место рождения:");
-            string[] item = Console.ReadLine().Split(',');
-            DateTime birthDate = DateTime.Parse(item[2]);
-            var totalmonths = (DateTime.Now.Year - birthDate.Year) * 12 + DateTime.Now.Month - birthDate.Month;
-            totalmonths += DateTime.Now.Day < birthDate.Day ? -1 : 0;
-            return new Worker()
-            {
-                Id = id,
-                DateAdd = dateAdd,
-                Fio = item[0],
-                Age = totalmonths / 12,
-                Height = Int32.Parse(item[1]),
-                BirthDate = birthDate, // почему то возвращает со временем
-                BirthPlace = item[3]
-            };
-        }
-        public static Worker AddWorker(string[] item)
-        {
-            return new Worker()
-            {
-                Id = Int32.Parse(item[0]),
-                DateAdd = DateTime.Parse(item[1]),
-                Fio = item[2],
-                Age = Int32.Parse(item[3]),
-                Height = Int32.Parse(item[4]),
-                BirthDate = DateTime.Parse(item[5]), // почему то возвращает со временем
-                BirthPlace = item[6]
-            };
-        }
-
     }
 }
--- Spravochnik/Repository.cs
+++ Spravochnik/Repository.cs
@@ -11,7 +11,7 @@ namespace Spravochnik
             int newId;
             int[] ids = (Workers != null) ? Workers.Select(w => w.Id).ToArray() : new int[1] { 0 };
             newId = (ids[0] == 0) ? 1 : ids.OrderBy(i => i).Reverse().First() + 1;            
-            Worker worker = Worker.AddWorker(newId, DateTime.Now);
+            Worker worker = AddWorker(newId, DateTime.Now);
             if (Workers != null)
                 Workers.Add(worker);
             else Workers = new List<Worker>() { worker };
@@ -53,18 +53,49 @@ namespace Spravochnik
             if (!(lines.Count > 0))
                 return null;
             foreach (var item in lines)
-                workers.Add(Worker.AddWorker(item));
+                workers.Add(AddWorker(item));
             return workers;
         }
         public void EditWorkerById(int id)
         {
             var workerIndex = Workers.FindIndex(w => w.Id == id);
             if (workerIndex == -1) throw new Exception();
-            Workers[workerIndex] = Worker.AddWorker(Workers[workerIndex].Id, Workers[workerIndex].DateAdd);
+            Workers[workerIndex] = AddWorker(Workers[workerIndex].Id, Workers[workerIndex].DateAdd);
         }
         public Worker GetWorkerById(int id) => Workers.Find(w => w.Id == id);
         public List<Worker> GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo) => Workers.Where(w => w.DateAdd >= dateFrom && w.DateAdd <= dateTo).ToList();
         public void DeleteWorker(int id) => Workers.RemoveAll(w => w.Id == id);
         public List<Worker> ShowByBirthDate() => Workers.OrderBy(w => w.BirthDate).ToList();
+        private static Worker AddWorker(int id, DateTime dateAdd)
+        {
+            Console.WriteLine("Введите через запятую(после запятой пробел не ставить) ФИО, рост, дату и место рождения:");
+            string[] item = Console.ReadLine().Split(',');
+            DateTime birthDate = DateTime.Parse(item[2]);
+            var totalmonths = (DateTime.Now.Year - birthDate.Year) * 12 + DateTime.Now.Month - birthDate.Month;
+            totalmonths += DateTime.Now.Day < birthDate.Day ? -1 : 0;
+            return new Worker()
+            {
+                Id = id,
+                DateAdd = dateAdd,
+                Fio = item[0],
+                Age = totalmonths / 12,
+                Height = Int32.Parse(item[1]),
+                BirthDate = birthDate, // почему то возвращает со временем
+                BirthPlace = item[3]
+            };
+        }
+        private static Worker AddWorker(string[] item)
+        {
+            return new Worker()
+            {
+                Id = Int32.Parse(item[0]),
+                DateAdd = DateTime.Parse(item[1]),
+                Fio = item[2],
+                Age = Int32.Parse(item[3]),
+                Height = Int32.Parse(item[4]),
+                BirthDate = DateTime.Parse(item[5]), // почему то возвращает со временем
+                BirthPlace = item[6]
+            };
+        }
     }
 }
